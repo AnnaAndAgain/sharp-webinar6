3a73d789c68fc7c3767d53e0d2b05b0cb28890cf
Anna <a.s.markovskaya@gmail.com>
2023-07-11 00:58:59 +0300

    webinar ended

--- task44_Fibonacci_no_recursion/Program.cs
+++ task44_Fibonacci_no_recursion/Program.cs
@@ -6,7 +6,7 @@ N = 5 -> 0 1 1 2 3
 N = 7 -> 0 1 1 2 3 5 8
 */
 
-int ReadInt(string text) 
+int ReadInt(string text)
 {
     Console.WriteLine(text);
     return Convert.ToInt32(Console.ReadLine());
@@ -20,15 +20,26 @@ void PrintArraySpace(int[] tmpArray)
 int[] FibonacciFill(int len)
 {
     int[] FiboArray = new int[len];
-    FiboArray[0]=0;
-    FiboArray[1]=1;
-    for (int i = 2; i < len; i++)
+    if (len == 1)
     {
-        FiboArray[i] = FiboArray[i-1] + FiboArray[i-2];
+        FiboArray[0] = 0;
+    }
+    else
+    {
+        FiboArray[0] = 0;
+        FiboArray[1] = 1;
+        for (int i = 2; i < len; i++)
+        {
+            FiboArray[i] = FiboArray[i - 1] + FiboArray[i - 2];
+        }
     }
     return FiboArray;
 }
 
 int N = ReadInt("Сколько чисел Фибоначчи вам вывести?");
-int[] array = FibonacciFill(N);
-PrintArraySpace(array);
+if (N <= 0) System.Console.WriteLine("Меньше 1 числа я вывести не могу!");
+else
+{
+    int[] array = FibonacciFill(N);
+    PrintArraySpace(array);
+}
--- task45_copy_array_elem_by_elem/Program.cs
+++ task45_copy_array_elem_by_elem/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Поэлементное копирование массива
+*/
+
+
+int[] FillArrayRandom(int tmpLength, int tmpMin, int tmpMax)
+{
+    int[] tmpArray = new int[tmpLength];
+    Random randnum = new Random();
+
+    for (int i = 0; i < tmpArray.Length; i++)
+    {
+        tmpArray[i] = randnum.Next(tmpMin, tmpMax + 1);
+    };
+
+    return tmpArray;
+}
+
+void PrintArray(int[] tmpArray)
+{
+    Console.WriteLine($"[" + string.Join(", ", tmpArray) + "]");
+}
+
+int[] CopyArray(int[] ArrayToCopy)
+{
+    int[] resultArray = new int[ArrayToCopy.Length];
+    for (int i =0; i< ArrayToCopy.Length; i++)
+    {
+        resultArray[i] = ArrayToCopy[i];
+    }
+    return resultArray;
+}
+
+int[] array1 = FillArrayRandom(10, -100, 100);
+int[] array2 = CopyArray(array1);
+//array1[0] = 300;
+PrintArray(array1);
+PrintArray(array2);
